3c3958583eaec6b204ad5f74a57500831edd427b
Shreyashee Bhattacharjee <shreyasheeb@gmail.com>
2018-05-19 18:35:16 -0400

    nav bar - v1

---
+++ app/src/main/java/com/sssb/orchard/SearchBills.java
@@ -0,0 +1,22 @@
+package com.sssb.orchard;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shreya on 5/19/18.
+ */
+
+public class SearchBills extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.search_bills, container, false);
+        return myView;
+    }
+}
